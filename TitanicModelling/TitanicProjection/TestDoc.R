51fa5018f397715e49acc0002edd170a74390930
padma <padma@srikanths-mbp.attlocal.net>
2017-08-02 01:44:19 -0700

    as numeric added, fare removed

--- TitanicModelling/TitanicProjection/TestDoc.R
+++ TitanicModelling/TitanicProjection/TestDoc.R
@@ -1,6 +1,13 @@
 
 #filling in the gaps for the ages, by putting in the mean of the rest
 Titanic_Raw$Age[is.na(Titanic_Raw$Age)] <- mean(Titanic_Raw$Age,na.rm=T)
+Titanic_Raw$Sex <- gsub(",","", Titanic_Raw$Sex)
+Titanic_Raw$Sex[Titanic_Raw$Sex == "male"] <- 0
+Titanic_Raw$Sex[Titanic_Raw$Sex == "female"] <- 1
+Titanic_Raw$Embarked[Titanic_Raw$Embarked == "C" | Titanic_Raw$Embarked == "c"] <- 0
+Titanic_Raw$Embarked[Titanic_Raw$Embarked == "S" | Titanic_Raw$Embarked == "s"] <- 1
+Titanic_Raw$Embarked[Titanic_Raw$Embarked == "Q" | Titanic_Raw$Embarked == "q"] <- 2
+
 #choosing the training data by randomly selecting 800 rows
 training_data2 <- Titanic_Raw[sample(nrow(Titanic_Raw), 800), ]
 
@@ -20,10 +27,11 @@ for (id in orig_data_ids) {
 
 #extracting the testing data using the testing id list and the original data
 testing_data4 <- subset(Titanic_Raw, PassengerId %in% testing_data_ids)
+
 #selecting all the pertaining columns, exlcuding unneccesary categorical variables,
 #such as fares, ticket numbers, names, etc
-training_data3 <- subset(training_data2,select=c(2,3,5,6,7,8,10,12))
-testing_data5 <- subset(testing_data4,select=c(2,3,4,5,6,7,8,10,12))
+training_data3 <- subset(training_data2,select=c(2,3,5,6,7,8,12))
+testing_data5 <- subset(testing_data4,select=c(2,3,4,5,6,7,8,12))
 
 #filling all the remaining empty spaces in the training and test data with NA
 is.na(training_data3) <- training_data3==''
